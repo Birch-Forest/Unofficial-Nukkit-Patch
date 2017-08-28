4623b5b798ed1fda2e8ab653fc0a9ff79192281f
Shanwer <Shanwer@qq.com>
2017-08-28 23:37:20 +0800

    Add new function--Fix Creative Inventory
    NO NEW COMMAND ADDED

--- src/main/java/com/Shanwer/Main/Patch.java
+++ src/main/java/com/Shanwer/Main/Patch.java
@@ -3,6 +3,9 @@ package com.Shanwer.Main;
 import cn.nukkit.Player;
 import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerJoinEvent;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.TextFormat;
 
@@ -12,9 +15,10 @@ import cn.nukkit.utils.TextFormat;
  * Last commit on 2017/8/26
  * Developers:Shanwer
  */
-public class Patch extends PluginBase {
+public class Patch extends PluginBase implements Listener{
     @Override
     public void onEnable() {
+        this.getServer().getPluginManager().registerEvents(new Patch(),this);
         this.getLogger().warning(TextFormat.GREEN + "Unofficial Nukkit Patch enabled!Author:Shanwer");
     }
 
@@ -23,5 +27,12 @@ public class Patch extends PluginBase {
         if (sender.isPlayer()) ((Player) sender).usedChunks.clear();
         sender.sendMessage(TextFormat.colorize("&3&b 区块已重发！"));
         return true;
+    }
+    @EventHandler//监听玩家进入服务器然后修复空背包
+    public void onPlayerJoin(PlayerJoinEvent event){
+        int fixgamemode = event.getPlayer().getGamemode();
+      if(fixgamemode == 1)
+        event.getPlayer().setGamemode(0);
+        event.getPlayer().setGamemode(1);
     }
         }
